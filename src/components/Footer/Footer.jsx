c5822ab49e2442f332f07cedc4c5705654b4e81a
L1kkkk <kn23-m.rudenko@nubip.edu.ua>
2025-02-20 12:31:24 +0200

    react-first-lab

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Це футер сайту</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import styles from "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <p>Це головна інфа сайту</p>
+      <button> Кнопка </button>
+    </main>
+  );
+}
+
+export default Main;
